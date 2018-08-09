24e441fd8751850f5c21d3e7faf5532a8c22c9c2
Federico <federico.valeri@studenti.unicam.it>
2018-08-09 18:29:49 +0200

    Adding the POST Backend Point

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-        "Access-Control-Allow-Header",
-        "Origin, X-Requested-Width, Content-Type, Accept"
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
     );
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PATCH, DELETE, PUT, OPTIONS"
     );
+    next();
 });
 
-app.use('/api/products', (req, res, next) => {
-    const products = [{
-        id: 'dsfsdgsbg',
-        title: 'First product',
-        price: 100,
-        description: 'toro loco is the best pizza ever'
-    }];
+
+app.post('/api/products', (req, res, next) => {
+    const product = req.body;
+    console.log(product);
+    res.status(201).json({
+        message: 'Post added succesfully'
+    });
+});
+
+app.get('/api/products', (req, res, next) => {
     res.status(200).json({
         message: 'Products fetched succesfully!',
         products: products
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -4,33 +4,41 @@ import { Subject } from 'rxjs';
 
 import { Product } from './product.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ProductsService {
-   private products: Product[] = [];
-   private productsUpdated = new Subject<Product[]>();
+  private products: Product[] = [];
+  private productsUpdated = new Subject<Product[]>();
 
-   constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-   getProducts() {
-     this.http.get<{message: string, products: Product[]}>('http://localhost:3000/api/products')
-     .subscribe((productData) => {
+  getProducts() {
+    this.http
+      .get<{ message: string; products: Product[] }>(
+        'http://localhost:3000/api/products'
+      )
+      .subscribe(productData => {
         this.products = productData.products;
         this.productsUpdated.next([...this.products]);
-     });
-   }
+      });
+  }
 
-   getProductUpdateListener() {
-     return this.productsUpdated.asObservable();
-   }
+  getProductUpdateListener() {
+    return this.productsUpdated.asObservable();
+  }
 
-   addProduct(title: string, price: number, description: string) {
-      const product: Product = {
-        id: null,
-        title: title,
-        price: price,
-        description: description
-      };
-      this.products.push(product);
-      this.productsUpdated.next([...this.products]);
-   }
+  addProduct(title: string, price: number, description: string) {
+    const product: Product = {
+      id: null,
+      title: title,
+      price: price,
+      description: description
+    };
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/products', product)
+      .subscribe(responseData => {
+        console.log(responseData.message);
+        this.products.push(product);
+        this.productsUpdated.next([...this.products]);
+      });
+  }
 }
