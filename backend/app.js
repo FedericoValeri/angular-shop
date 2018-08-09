b18e78ccdfc9093197f57a8f414f444293de9c06
Federico <federico.valeri@studenti.unicam.it>
2018-08-09 14:56:49 +0200

    Handling CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Header",
+        "Origin, X-Requested-Width, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, PUT, OPTIONS"
+    );
+});
+
 app.use('/api/products', (req, res, next) => {
     const products = [{
         id: 'dsfsdgsbg',
